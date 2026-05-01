679cd3d4e5446f9c80e30570343781d9be3b7f0e
Raneem Ali <raneem309@gmail.com>
2026-05-01 15:34:42 -0400

    update

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/ReactProject002/",
+  base: "/ReactProject003/",
   plugins: [react()],
 });
